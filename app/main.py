4fbf983d5aaff3d55d0a5244d4854e3c2e91107f
cloudstacker <iamjayakrishna7@gmail.com>
2025-08-29 14:42:44 +0530

    Updated main.py to display clear application instance name to understand load balancing

--- app/main.py
+++ app/main.py
@@ -1,9 +1,14 @@
 from flask import Flask, render_template
 import routes
+import os
 
 app = Flask(__name__)
 
 app.register_blueprint(routes.main)
+
+@app.context_processor
+def inject_app_name():
+    return dict(app_name=os.getenv("APP_NAME", "Flask-App"))
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
